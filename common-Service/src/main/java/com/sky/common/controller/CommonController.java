d69362435bf5fcb76cb77c7e4faada6d7ad03d3f
Jerry <2628392191@qq.com>
2026-04-15 20:12:49 +0800

    4月15日：补充

---
+++ common-Service/src/main/java/com/sky/common/controller/CommonController.java
@@ -0,0 +1,34 @@
+package com.sky.common.controller;
+
+import com.itheima.alioss.AliOSSUtils;
+import com.sky.gateway.utils.common.result.Result;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/admin/common")
+@Api(tags="通用管理")
+@Slf4j
+public class CommonController {
+    @Autowired
+    private AliOSSUtils aliOSSUtils;
+
+
+    //图片能上传但无法显示
+    @ApiOperation("文件上传")
+    @PostMapping("/upload")
+    public Result upload(MultipartFile file) throws IOException {
+         log.info("文件上传：{}", file);
+         String path=aliOSSUtils.upload(file);
+         return Result.success(path);
+     }
+
+}
